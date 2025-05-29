140448e3a8b365127ba9570e8a9508067ddcc1aa
ww-albij <albij.madhikarmi@whatworks.com.au>
2025-05-29 16:02:54 +0545

    ui/parse_link => added container with border to represent a link - done

--- lib/src/widget/parse_link.dart
+++ lib/src/widget/parse_link.dart
@@ -5,19 +5,39 @@ InlineSpan _parseLink(Map<String, dynamic> child, TextStyle textStyle,
   final int? format = child["format"] is int? ? child["format"] : 0;
   final baseStyle = _textStyle(format, textStyle, useMyStyle);
 
-  final String link = child["fields"]["url"] as String;
+  final String? link = child?["fields"]?["url"] as String?;
 
   handleOpenLink() async {
-    await launchUrl(Uri.parse(link));
+    LaunchMode launchMode = child["fields"]["newTab"] == true
+        ? LaunchMode.externalApplication
+        : LaunchMode.platformDefault;
+
+    if (link != null) {
+      await launchUrl(Uri.parse(link), mode: launchMode);
+    }
   }
 
   final List<dynamic> childElements = child["children"] ?? [];
 
-  final List<InlineSpan> widgets = parseJsonChild(childElements, context);
+  final List<InlineSpan> widgets = parseJsonChild(
+      childElements.map((element) {
+        // element["format"] = 8;
+        return element;
+      }).toList(),
+      context);
 
   return WidgetSpan(
       child: GestureDetector(
     onTap: handleOpenLink,
-    child: RichText(text: TextSpan(children: [...widgets], style: baseStyle)),
+    child: Container(
+        decoration: BoxDecoration(
+            border: Border(
+                bottom:
+                    BorderSide(color: baseStyle.color ?? Color(0xFF000000)))),
+        child: RichText(
+            text: TextSpan(
+          style: baseStyle,
+          children: [...widgets],
+        ))),
   ));
 }
